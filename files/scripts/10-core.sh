80b57e4041ff841941e5e45f4a79c889bcbaa5bb
kinggrowler-macfed <king.growler+macfed@gmail.com>
2025-08-07 10:29:27 -0700

    clean some cruft, add a few more packages

--- files/scripts/10-core.sh
+++ files/scripts/10-core.sh
@@ -6,5 +6,5 @@ systemctl enable rpm-ostreed-automatic.timer
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-41.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ledif-skippy-xd-fedora-41.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-42.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ledif-skippy-xd-fedora-42.repo
--- files/scripts/11-main.sh
+++ files/scripts/11-main.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 # TODO: use systemd-tmpfiles for this
 echo "KillUserProcesses=yes" >>/usr/lib/systemd/logind.conf
 
-# this script from winblues no longer works, erroring with:
+# this tool from winblues no longer works, erroring with:
 # Error initializing Xfconf: xml: unsupported version "1.1"; only version 1.0 is supported
 #curl -L -o /usr/bin/xfconf-profile https://github.com/winblues/xfconf-profile/releases/latest/download/xfconf-profile-linux-amd64
 #chmod +x /usr/bin/xfconf-profile
@@ -18,6 +18,8 @@ ln -s /usr/lib/ruby/gems/fusuma/bin/fusuma /usr/bin/fusuma
 
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras}
 dnf5 -y install flatpost
+dnf5 -y install firacode-nerd-fonts
+dnf5 -y install nerdfontssymbolsonly-nerd-fonts
 dnf5 -y config-manager setopt "terra*".enabled=false
 
 systemctl preset-all
